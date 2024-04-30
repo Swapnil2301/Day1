0fea962554061654dce7b938a4986de115e0a743
YUVRAJ N JADHAV <80637788+yuvraj2675@users.noreply.github.com>
2024-04-30 15:23:47 +0530

    Update equality.java

--- equality.java
+++ equality.java
@@ -4,6 +4,8 @@ public class equality {
     public static void main(String[] args) {
         String x = "Hello";
         String y = "Helll";
+        String boy = "Boy" , girl = "girl";
+        System.out.println((boy == girl) ? "They are equal" : "Of course not, boys are superior and equality is a myth");
         boolean compare = x==y;
         System.out.println(compare);
     }
